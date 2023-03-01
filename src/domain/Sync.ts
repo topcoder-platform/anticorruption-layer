cab54170cf79e66c9636a0eb2699a097c57c190e
Rakib Ansary <rakibansary@gmail.com>
2023-03-02 02:51:47 +0600

    wip: add update stubs

--- src/domain/Sync.ts
+++ src/domain/Sync.ts
@@ -1,14 +1,15 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { QueryBuilder } from "@topcoder-framework/client-relational";
-import { ChallengeDomain } from "@topcoder-framework/domain-challenge";
+import {
+  Challenge,
+  ChallengeDomain,
+  UpdateChallengeInput_UpdateInput,
+} from "@topcoder-framework/domain-challenge";
 import _ from "lodash";
 import { queryRunner } from "../helper/QueryRunner";
 
-import { Challenge } from "@topcoder-framework/domain-challenge/dist-es/models/domain-layer/challenge/challenge";
-import { Operator } from "@topcoder-framework/lib-common";
+import { validateLoadBalancingConfig } from "@grpc/grpc-js/build/src/load-balancer";
+import { UpdateChallengeInput as V5UpdateChallengeInput } from "@topcoder-framework/domain-challenge";
+import { DomainHelper, Operator } from "@topcoder-framework/lib-common";
 import {
   ChallengeStatus,
   ChallengeStatusIds,
@@ -20,13 +21,13 @@ import { LegacyChallenge, LegacyChallengeId } from "../models/domain-layer/legac
 import { SyncInput, Table } from "../models/domain-layer/legacy/sync";
 
 const challengeDomain = new ChallengeDomain(
-  process.env.GRPC_CHALLENGE_DOMAIN_SERVER_HOST,
-  process.env.GRPC_CHALLENGE_DOMAIN_SERVER_PORT
+  process.env.GRPC_CHALLENGE_DOMAIN_SERVER_HOST as string,
+  process.env.GRPC_CHALLENGE_DOMAIN_SERVER_PORT as string
 );
 
 class LegacySyncDomain {
   public async syncLegacy(input: SyncInput): Promise<void> {
-    const legacyId = input2.projectId;
+    const legacyId = input.projectId;
     const payload = {};
     const legacyChallenge = await LegacyChallengeDomain.getLegacyChallenge(
       LegacyChallengeId.create({ legacyChallengeId: legacyId })
@@ -35,12 +36,24 @@ class LegacySyncDomain {
     const { items } = await challengeDomain.scan({
       criteria: [{ key: "legacyId", operator: Operator.OPERATOR_EQUAL, value: legacyId }],
     });
-    const v5Challenge: Challenge = items[0];
+
+    if (items.length === 0) {
+      throw new Error("Challenge not found");
+    }
+    const v5ChallengeUpdates: Partial<UpdateChallengeInput_UpdateInput> = {};
+    const v5ChallengeUpdateInput: Partial<V5UpdateChallengeInput> = {
+      filterCriteria: [
+        {
+          key: "id",
+          operator: Operator.OPERATOR_EQUAL,
+          value: (items[0] as { id: string }).id,
+        },
+      ],
+    };
 
     for (const table of input.updatedTables) {
       switch (table.table) {
         case "project":
-          _.assign(payload, this.handleProjectUpdate(table.value, legacyChallenge, v5Challenge));
           break;
         case "project_phase":
           break;
@@ -59,43 +72,45 @@ class LegacySyncDomain {
         default:
       }
     }
+
+    // set v5ChallengeUpdates to v5ChallengeUpdateInput
   }
 
-  private handleProjectUpdate(
-    columnNames: string[],
-    legacyChallenge: LegacyChallenge,
-    v5Challenge: Challenge
-  ) {
-    const payload = {};
-    for (const columnName of columnNames) {
-      switch (columnName) {
-        case "project_status_id":
-          _.assign(payload, this.handleStatusChange(legacyChallenge, v5Challenge));
-          break;
-        case "project_category_id":
-          break;
-        case "tc_direct_project_id":
-          break;
-        default:
-      }
-    }
-  }
+  // private handleProjectUpdate(
+  //   columnNames: string[],
+  //   legacyChallenge: LegacyChallenge,
+  //   v5Challenge: Challenge
+  // ) {
+  //   const payload = {};
+  //   for (const columnName of columnNames) {
+  //     switch (columnName) {
+  //       case "project_status_id":
+  //         _.assign(payload, this.handleStatusChange(legacyChallenge, v5Challenge));
+  //         break;
+  //       case "project_category_id":
+  //         break;
+  //       case "tc_direct_project_id":
+  //         break;
+  //       default:
+  //     }
+  //   }
+  // }
 
-  private handleStatusChange(legacyChallenge: LegacyChallenge, v5Challenge: Challenge) {
-    const v4StatusNumber =
-      ChallengeStatusOrders[
-        ChallengeStatusMap[legacyChallenge.projectStatusId as ChallengeStatusIds] as ChallengeStatus
-      ] || ChallengeStatusOrders.Cancelled;
-    const v5StatusNumber =
-      ChallengeStatusOrders[v5Challenge.status as ChallengeStatus] ||
-      ChallengeStatusOrders.Cancelled;
+  // private handleStatusChange(legacyChallenge: LegacyChallenge, v5Challenge: Challenge) {
+  //   const v4StatusNumber =
+  //     ChallengeStatusOrders[
+  //       ChallengeStatusMap[legacyChallenge.projectStatusId as ChallengeStatusIds] as ChallengeStatus
+  //     ] || ChallengeStatusOrders.Cancelled;
+  //   const v5StatusNumber =
+  //     ChallengeStatusOrders[v5Challenge.status as ChallengeStatus] ||
+  //     ChallengeStatusOrders.Cancelled;
 
-    if (v4StatusNumber >= v5StatusNumber) {
-      return { status: ChallengeStatusMap[legacyChallenge.projectStatusId as ChallengeStatusIds] };
-    } else {
-      return {};
-    }
-  }
+  //   if (v4StatusNumber >= v5StatusNumber) {
+  //     return { status: ChallengeStatusMap[legacyChallenge.projectStatusId as ChallengeStatusIds] };
+  //   } else {
+  //     return {};
+  //   }
+  // }
 }
 
 export default new LegacySyncDomain();
